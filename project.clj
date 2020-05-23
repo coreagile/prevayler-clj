ac57cbd9c0bad457d882963e0181da7dd667fafc
sstarkey <sstarkey@opploans.com>
2020-05-23 10:21:45 -0500

    Snapshotting

--- project.clj
+++ project.clj
@@ -1,11 +1,10 @@
-(defproject coreagile/prevayler-clj "3.0.6"
+(defproject coreagile/prevayler-clj "3.0.7-SNAPSHOT"
   :description "Simple, fast, ACID persistence in Clojure."
   :url "https://github.com/klauswuestefeld/prevayler-clj"
   :license {:name "BSD"
             :url "http://opensource.org/licenses/BSD-3-Clause"}
-  :dependencies [[org.clojure/clojure "1.10.1"]
+  :dependencies [[org.clojure/clojure "1.10.1" :scope "provided"]
                  [com.taoensso/nippy "2.14.0"]
-
                  [software.amazon.awssdk/s3 "2.13.23"
                   :scope "provided"]]
   :profiles {:dev {:dependencies [[midje "1.9.9"]]
