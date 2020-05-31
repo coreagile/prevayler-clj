4d4f0eed7506471cc2bf9ac1db6e9d56d4762f13
sstarkey <sstarkey@opploans.com>
2020-05-31 11:58:42 -0500

    New version

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject coreagile/prevayler-clj "3.0.11"
+(defproject coreagile/prevayler-clj "3.0.12-SNAPSHOT"
   :description "Simple, fast, ACID persistence in Clojure."
   :url "https://github.com/klauswuestefeld/prevayler-clj"
   :license {:name "BSD"
